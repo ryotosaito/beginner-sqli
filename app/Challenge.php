b5c3c61f78541ace96820bbacfa213e6436c4169
Ryoto Saito <ryoto.no.address@gmail.com>
2018-05-14 23:26:29 +0900

    [add] many-many relations

--- app/Challenge.php
+++ app/Challenge.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Challenge extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    private function solvers() {
+        return $this->belongsToMany('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    public function solves() {
+        return $this->hasMany('challenges');
+    }
 }

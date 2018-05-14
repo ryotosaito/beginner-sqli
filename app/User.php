0dfe71e8276b0c8d802367bc633e924d6e143294
Ryoto Saito <ryoto.no.address@gmail.com>
2018-05-14 10:34:40 +0900

    [update] modify default migration

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
     ];
 }

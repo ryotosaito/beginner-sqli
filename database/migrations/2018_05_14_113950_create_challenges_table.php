b663eb32bb25dbd377d6443ad951b9f377376a19
Ryoto Saito <ryoto.no.address@gmail.com>
2018-05-14 21:03:30 +0900

    [add] create challenges table

---
+++ database/migrations/2018_05_14_113950_create_challenges_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('category', '64');
+            $table->unsignedInteger('number');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->unique(['category', 'number'], 'unique_category_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}

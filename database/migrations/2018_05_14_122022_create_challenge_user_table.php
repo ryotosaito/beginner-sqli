273a51898fd6100813cb9c031a25148e82968da4
Ryoto Saito <ryoto.no.address@gmail.com>
2018-05-14 21:31:29 +0900

    [fix] rename database table

--- database/migrations/2018_05_14_122022_create_challenge_user_table.php
+++ database/migrations/2018_05_14_122022_create_challenge_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSolvesTable extends Migration
+class CreateChallengeUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSolvesTable extends Migration
      */
     public function up()
     {
-        Schema::create('solves', function (Blueprint $table) {
+        Schema::create('challenge_user', function (Blueprint $table) {
             $table->unsignedInteger('challenge_id');
             $table->unsignedInteger('user_id');
             $table->timestamps();
@@ -30,6 +30,6 @@ class CreateSolvesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('solves');
+        Schema::dropIfExists('challenge_user');
     }
 }

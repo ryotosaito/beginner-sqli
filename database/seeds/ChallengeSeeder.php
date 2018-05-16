9c11ba7fbbd3e1711719f0b399d6a5dfef1beed6
Ryoto Saito <ryoto.no.address@gmail.com>
2018-05-16 20:23:46 +0900

    [add] database seeder

---
+++ database/seeds/ChallengeSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ChallengeSeeder extends Seeder
+{
+    /**
+     * @param integer $number
+     * @param string $title
+     * @param string $description
+     * @param string $flag
+     */
+    private static function seed_tutorial($number, $title, $description, $flag)
+    {
+        DB::table('challenges')->insert([
+            'category' => 'tutorial',
+            'number' => $number,
+            'title' => $title,
+            'description' => $description,
+            'url' => env('CHALLENGE_URL')."/tutorial{$number}/",
+            'flag' => "m1z0r3{{$flag}}",
+        ]);
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        static::seed_tutorial(
+            1,
+            'A very beginning',
+            'Read database.',
+            'not_only_sqli_but_base64');
+        static::seed_tutorial(
+            2,
+            'm1z0r3 m1z0r3 m1z0r3',
+            'Read database again.',
+            'h3r3isth3r341f14g');
+        static::seed_tutorial(
+            3,
+            'Where is my partner?',
+            'Make two tables into one view',
+            'J4p4n3s3_wariin');
+    }
+}

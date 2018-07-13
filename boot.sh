79699b9347f87afc723c75dbff51a86049c3fdb8
Ryoto Saito <ryoto.no.address@gmail.com>
2018-07-13 16:16:51 +0900

    [update] mysql migration and seeding certainly

--- boot.sh
+++ boot.sh
@@ -1,4 +1,5 @@
 docker-compose up -d && \
+until docker-compose exec database mysql -uroot -p$(docker-compose exec database bash -c 'echo $MYSQL_ROOT_PASSWORD') -e";" &>/dev/null; do sleep 1; done && \
 docker-compose exec app php artisan key:generate && \
 docker-compose exec app php artisan migrate && \
 docker-compose exec app php artisan db:seed --class=ChallengeSeeder
